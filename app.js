8db50cdeea4248904afa5ce064d65872f4ac6ea6
Lukas Ruebbelke <simpul@gmail.com>
2015-01-07 07:32:04 -0700

    Cleaning things up.

--- app.js
+++ app.js
@@ -1,44 +1,44 @@
 angular.module('App', ['ngAnimate', 'mgcrea.ngStrap.modal'])
-.controller('AppCtrl', function($scope) {
-  $scope.steps = ['one', 'two', 'three'];
-  $scope.step = 0;
-  $scope.wizard = { tacos: 2 };
+    .controller('AppCtrl', function ($scope) {
+        $scope.steps = ['one', 'two', 'three'];
+        $scope.step = 0;
+        $scope.wizard = {tacos: 2};
 
-  $scope.isFirstStep = function() {
-    return $scope.step === 0;
-  };
+        $scope.isFirstStep = function () {
+            return $scope.step === 0;
+        };
 
-  $scope.isLastStep = function() {
-    return $scope.step === ($scope.steps.length - 1);
-  };
-  
-  $scope.isCurrentStep = function(step) {
-    return $scope.step === step;
-  };
+        $scope.isLastStep = function () {
+            return $scope.step === ($scope.steps.length - 1);
+        };
 
-  $scope.setCurrentStep = function(step) {
-    $scope.step = step;
-  };
+        $scope.isCurrentStep = function (step) {
+            return $scope.step === step;
+        };
 
-  $scope.getCurrentStep = function() {
-    return $scope.steps[$scope.step];
-  };
+        $scope.setCurrentStep = function (step) {
+            $scope.step = step;
+        };
 
-  $scope.getNextLabel = function() {
-    return ($scope.isLastStep()) ? 'Submit' : 'Next'; 
-  };
+        $scope.getCurrentStep = function () {
+            return $scope.steps[$scope.step];
+        };
 
-  $scope.handlePrevious = function() {
-    $scope.step -= ($scope.isFirstStep()) ? 0 : 1;
-  };
+        $scope.getNextLabel = function () {
+            return ($scope.isLastStep()) ? 'Submit' : 'Next';
+        };
 
-  $scope.modal = {};
+        $scope.handlePrevious = function () {
+            $scope.step -= ($scope.isFirstStep()) ? 0 : 1;
+        };
 
-  $scope.handleNext = function(callback) {
-    if($scope.isLastStep()) {
-      callback();
-    } else {
-      $scope.step += 1;
-    }
-  };
-});
+        $scope.modal = {};
+
+        $scope.handleNext = function (callback) {
+            if ($scope.isLastStep()) {
+                callback();
+            } else {
+                $scope.step += 1;
+            }
+        };
+    });
