e57509502fc12146d4a66bf8f162ee7015169dc0
Lukas Ruebbelke <simpul@gmail.com>
2015-01-09 08:46:05 -0700

    Clean up

--- app.js
+++ app.js
@@ -2,26 +2,27 @@ angular.module('App', ['ngAnimate', 'ui.bootstrap'])
     .controller('AppCtrl', function ($scope, $modal) {
         var app = this;
 
-        app.closeAlert = function() {
+        app.closeAlert = function () {
             app.reason = null;
         };
 
-        app.open = function() {
-          var modalInstance = $modal.open({
-            templateUrl: "partials/wizard.html",
-            controller: "ModalCtrl",
-            controllerAs: "modal",
-          });
+        app.open = function () {
+            var modalInstance = $modal.open({
+                templateUrl: 'partials/wizard.html',
+                controller: 'ModalCtrl',
+                controllerAs: 'modal'
+            });
 
-          modalInstance.result.then(function(data) {
-            app.closeAlert();
-            app.summary = data;
-          }, function(reason) {
-            app.reason = reason;
-          })
+            modalInstance.result
+                .then(function (data) {
+                    app.closeAlert();
+                    app.summary = data;
+                }, function (reason) {
+                    app.reason = reason;
+                });
         };
     })
-    .controller('ModalCtrl', function($scope, $modalInstance) {
+    .controller('ModalCtrl', function ($modalInstance) {
         var modal = this;
 
         modal.steps = ['one', 'two', 'three'];
@@ -29,38 +30,38 @@ angular.module('App', ['ngAnimate', 'ui.bootstrap'])
         modal.wizard = {tacos: 2};
 
         modal.isFirstStep = function () {
-          return modal.step === 0;
+            return modal.step === 0;
         };
 
         modal.isLastStep = function () {
-          return modal.step === (modal.steps.length - 1);
+            return modal.step === (modal.steps.length - 1);
         };
 
         modal.isCurrentStep = function (step) {
-          return modal.step === step;
+            return modal.step === step;
         };
 
         modal.setCurrentStep = function (step) {
-          modal.step = step;
+            modal.step = step;
         };
 
         modal.getCurrentStep = function () {
-          return modal.steps[modal.step];
+            return modal.steps[modal.step];
         };
 
         modal.getNextLabel = function () {
-          return (modal.isLastStep()) ? 'Submit' : 'Next';
+            return (modal.isLastStep()) ? 'Submit' : 'Next';
         };
 
         modal.handlePrevious = function () {
-          modal.step -= (modal.isFirstStep()) ? 0 : 1;
+            modal.step -= (modal.isFirstStep()) ? 0 : 1;
         };
 
-        modal.handleNext = function (callback, data) {
-          if (modal.isLastStep()) {
-            callback(data);
-          } else {
-            modal.step += 1;
-          }
+        modal.handleNext = function (data) {
+            if (modal.isLastStep()) {
+                $modalInstance.close(data);
+            } else {
+                modal.step += 1;
+            }
         };
     });
