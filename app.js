df26f1ba059172084275a484155c9e5465e3da3f
Kent C. Dodds <kent+github@doddsfamily.us>
2015-01-12 14:49:48 -0700

    New code style
    I like this because I can look at the top of the file and see exactly what functions/variables I can use in the template. You could also use `angular.extend` so you don't have to type `modal.` everywhere.
    
    Also, doing things this way encourages you to make sure that you hoist all variables yourself (and all functions are auto-hoisted anyway).
    
    Also, using function declarations like this assign the `function.name` property to the name of the function which is useful in debugging (though I believe the latest versions of Chrome will infer the name based on the name of the variable you assign the expression to, which is mind blowing).

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 angular.module('App', ['ngAnimate', 'ui.bootstrap'])
     .controller('AppCtrl', function ($modal) {
         var app = this;
-
-        app.closeAlert = function () {
+        
+        // function assignment
+        app.closeAlert = closeAlert;
+        app.open = open;
+        
+        // function declaration
+        function closeAlert() {
             app.reason = null;
         };
 
-        app.open = function () {
+        function open() {
             var modalInstance = $modal.open({
                 templateUrl: 'partials/wizard.html',
                 controller: 'ModalCtrl',
@@ -25,47 +30,57 @@ angular.module('App', ['ngAnimate', 'ui.bootstrap'])
     .controller('ModalCtrl', function ($modalInstance) {
         var modal = this;
 
+        // variable assignment
         modal.steps = ['one', 'two', 'three'];
         modal.step = 0;
         modal.wizard = {tacos: 2};
-
-        modal.isFirstStep = function () {
+        
+        // function assignment
+        modal.isFirstStep = isFirstStep;
+        modal.isLastStep = isLastStep;
+        modal.isCurrentStep = isCurrentStep;
+        modal.setCurrentStep = setCurrentStep;
+        modal.getCurrentStep = getCurrentStep;
+        modal.getNextLabel = getNextLabel;
+        modal.handlePrevious = handlePrevious;
+        modal.handleNext = handleNext;
+        modal.dismiss = $modalInstance.dismiss;
+        
+        
+        // function declaration
+        function isFirstStep() {
             return modal.step === 0;
-        };
+        }
 
-        modal.isLastStep = function () {
+        function isLastStep() {
             return modal.step === (modal.steps.length - 1);
-        };
+        }
 
-        modal.isCurrentStep = function (step) {
+        function isCurrentStep(step) {
             return modal.step === step;
-        };
+        }
 
-        modal.setCurrentStep = function (step) {
+        function setCurrentStep(step) {
             modal.step = step;
-        };
+        }
 
-        modal.getCurrentStep = function () {
+        function getCurrentStep() {
             return modal.steps[modal.step];
-        };
+        }
 
-        modal.getNextLabel = function () {
+        function getNextLabel() {
             return (modal.isLastStep()) ? 'Submit' : 'Next';
-        };
+        }
 
-        modal.handlePrevious = function () {
+        function handlePrevious() {
             modal.step -= (modal.isFirstStep()) ? 0 : 1;
-        };
+        }
 
-        modal.handleNext = function () {
+        function handleNext() {
             if (modal.isLastStep()) {
                 $modalInstance.close(modal.wizard);
             } else {
                 modal.step += 1;
             }
-        };
-
-        modal.dismiss = function(reason) {
-            $modalInstance.dismiss(reason);
-        };
+        }
     });
